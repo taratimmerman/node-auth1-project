e9af35e82e25e503dd6b8550c168b19b72e5bb05
taratimmerman <timmermantara@gmail.com>
2021-02-22 15:28:38 -0700

    project set up

---
+++ database/migrations/20210222150449_create.js
@@ -0,0 +1,21 @@
+
+exports.up = function (knex) {
+    return knex.schema
+        .createTable("users", tbl => {
+            tbl.increments();
+
+            tbl.string("username", 128).notNullable().unique().index();
+            tbl.string("password", 256).notNullable();
+
+            tbl
+                .integer("role")
+                .unsigned()
+                .references("roles.id")
+                .onDelete("RESTRICT")
+                .onUpdate("CASCADE");
+        });
+};
+
+exports.down = function (knex) {
+    return knex.schema.dropTableIfExists("roles").dropTableIfExists("users");
+};
